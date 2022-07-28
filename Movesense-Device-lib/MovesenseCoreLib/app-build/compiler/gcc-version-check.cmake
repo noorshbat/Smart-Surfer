1cb46ceb34f2b940a2b054f77509684f6bd0b019
noorshbat <96077234+noorshbat@users.noreply.github.com>
2022-07-28 18:13:33 +0300

    Delete Movesense-Device-lib directory

--- Movesense-Device-lib/MovesenseCoreLib/app-build/compiler/gcc-version-check.cmake
+++
@@ -1,6 +0,0 @@
-if (NOT CMAKE_C_COMPILER_VERSION VERSION_EQUAL 9.2.1)
-    message(FATAL_ERROR "GCC C compiler version must be 9.2.1! --- (${CMAKE_C_COMPILER_VERSION} detected)")
-endif()
-if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_EQUAL 9.2.1)
-    message(FATAL_ERROR "GCC C++ compiler version must be 9.2.1! --- (${CMAKE_CXX_COMPILER_VERSION} detected)")
-endif()
